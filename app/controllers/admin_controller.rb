9e657e328e1cb5624434c4d59f0519409ffcb769
Stefflan00 <stefan.maier09@gmail.com>
2014-10-06 21:05:18 +0200

    authentification

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
 layout 'admin'
-
+http_basic_authenticate_with :name => "frodo", :password => "thering"  
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+   http_basic_authenticate_with :name => "frodo", :password => "thering"  
   def startpage
   end
 end
