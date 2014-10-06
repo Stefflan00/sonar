aacef3eb02747b4c28c34c3908c6187147e2d65c
Stefflan00 <stefan.maier09@gmail.com>
2014-10-06 10:13:20 +0200

    some test with apple api

--- app/controllers/landingpages_controller.rb
+++ app/controllers/landingpages_controller.rb
@@ -1,10 +1,18 @@
 class LandingpagesController < ApplicationController
+require 'open-uri'
+require 'json'
+
 layout 'ios_landingpage', :only => [:games]
 
 def games
 
+@object = JSON.parse(open("https://itunes.apple.com/lookup?id=774751518").read)
+#@new_object = ActiveSupport::JSON.decode(@object)
 
 end
+
+
+
 
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+
+def apple_lookup id
+  "https://itunes.apple.com/lookup?id=#{id}"
+end
+
+
+def app id
+  return object = JSON.parse(open("https://itunes.apple.com/lookup?id=#{id}").read)['results']
+end
+
+def app_price id
+  app(id).first['price']
+end
+
+
 end
